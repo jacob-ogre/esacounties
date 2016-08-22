f44137da9babe2ac9326b327bc29a103dac2f5cf
Jacob Malcom <jmalcom@defenders.org>
2016-08-22 06:21:42 -0400

    Add esacounties data to package.

--- data-raw/load_data.R
+++ data-raw/load_data.R
@@ -1,2 +1,18 @@
 # BSD_2_clause
 
+data <- readr::read_tsv("data-raw/EndSp_county_occurrences.tsv")
+dim(data)
+names(data)
+head(data)
+head(data.frame(data))
+
+names(data)[1] <- "Scientific"
+names(data)[4] <- "County_name"
+names(data)[5] <- "State_abbrev"
+names(data)[6] <- "State_name"
+names(data)[9] <- "Common"
+
+data <- data[, c(1, 13:15, 9:12, 2:8)]
+
+esacounties <- data
+devtools::use_data(esacounties, overwrite = TRUE)
