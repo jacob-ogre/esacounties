424cf6fd26b03a28c1d97c094fad718b87c7fa76
Jacob Malcom <jmalcom@defenders.org>
2016-09-23 06:43:48 -0400

    Update formatting in tests.

--- tests/testthat/test_counties.R
+++ tests/testthat/test_counties.R
@@ -7,12 +7,13 @@ LAblack <- get_counties("Ursus americanus luteolus")
 test_that("get_counties returns the correct data", {
   expect_is(grizz, "tbl_df")
   expect_equal(grizz$GEOID,
-               esacounties$GEOID[which(esacounties$Scientific == "Ursus arctos horribilis")])
-}
-)
+               esacounties$GEOID[which(esacounties$Scientific ==
+                                       "Ursus arctos horribilis")])
+})
 
 test_that("get_counties is flexible to scientific name input", {
   expect_equal(grizz, get_counties("ursus arctos horribilis"))
-  expect_equal(LAblack$Scientific %in% black$Scientific, rep(TRUE,nrow(LAblack)))
+  expect_equal(LAblack$Scientific %in% black$Scientific,
+               rep(TRUE,nrow(LAblack)))
 })
 
--- tests/testthat/test_species.R
+++ tests/testthat/test_species.R
@@ -2,12 +2,12 @@ context("Species")
 
 test_that("get_species works with state, county input", {
   fxn_data <- get_species(state = "NY", county = "Monroe")
-  tbl_sub <- which(esacounties$State_abbrev == "NY" & esacounties$County_name == "Monroe")
+  tbl_sub <- which(esacounties$State_abbrev == "NY" &
+                   esacounties$County_name == "Monroe")
   expect_is(fxn_data, "tbl_df")
   expect_equal(nrow(fxn_data),length(tbl_sub))
   expect_equal(fxn_data$GEOID, esacounties$GEOID[tbl_sub])
-}
-)
+})
 
 test_that("get_species works with FIPS input", {
   fxn_data <- get_species(fips = "48395")
@@ -16,6 +16,5 @@ test_that("get_species works with FIPS input", {
   expect_equal(nrow(fxn_data),length(tbl_sub))
   expect_equal(fxn_data$GEOID, esacounties$GEOID[tbl_sub])
   expect_error(get_species(fips = ""))
-}
-)
+})
 
